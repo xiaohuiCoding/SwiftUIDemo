7292e8cb6eff00e9ba459374d6befd3f039537e4
lyx <1525328738@qq.com>
2022-11-09 12:24:48 +0800

    create a project

---
+++ SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by apple on 2022/11/9.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
